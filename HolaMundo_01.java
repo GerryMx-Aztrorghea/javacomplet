1f0d47b8dcfb195ba1d8f1bf13ef9d1451170e6b
GerryMx-Aztrorghea <108512123+GerryMx-AZtrorghea@users.norepley.github.com>
2022-11-23 16:37:40 -0600

    Saludos desde java y imprimir texto en pantalla

---
+++ HolaMundo_01.java
@@ -0,0 +1,9 @@
+// Mi clase de Java
+
+public class HolaMundo {
+
+    public static void main(String args[]){
+
+        System.out.println("Hola mundo desde Java");
+    }
+}
---
+++ SaludarJava_02.java
@@ -0,0 +1,10 @@
+// Enviando un salido desde java
+
+public class SaludarJava {
+
+    public static void main(String arg[]){
+
+        System.out.println("Hola aqui comenzando con JAVA, se manda un cordial saludo");
+
+        }
+}
